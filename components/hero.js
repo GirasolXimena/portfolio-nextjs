f6c2a0f547241528ef0d47e59f996f4d6de3da33
Robert Andrade <srandradejr@gmail.com>
2021-12-30 17:58:22 +0000

    added hero skeleton

---
+++ components/hero.js
@@ -0,0 +1,8 @@
+export default function Hero() {
+  return (
+    <>
+      <h1>S. Roberto Andrade</h1>
+      <h2>Creative Technologist</h2>
+    </>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout, { siteTitle } from '../components/layout'
+import Hero from '../components/hero'
 import utilStyles from '../styles/utils.module.css'
 
 export default function Home() {
@@ -9,13 +10,14 @@ export default function Home() {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      <section className={utilStyles.headingMd}>
+      <main className={utilStyles.headingMd}>
+        <Hero />
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-      </section>
+      </main>
       <Link href="/resume"><a>View Resume</a></Link>
     </Layout>
   )
