0dcd19eba1ed97846c3c926387d11f6a00a11c07
Robert Andrade <srandradejr@gmail.com>
2022-01-02 05:05:15 +0000

    update resume page formatting

--- pages/resume.js
+++ pages/resume.js
@@ -4,13 +4,17 @@ import ResumeTemplate from '../components/resumeTemplate';
 import utilStyles from '../styles/utils.module.scss';
 
 export default function Resume() {
-  const [pretty, setPretty] = useState(false);
+  const [pretty, setPretty] = useState(true);
 
   return (
     <Layout>
-      <button className={utilStyles.noPrint} onClick={() => setPretty(!pretty)}>toggle pretty</button>
       <ResumeTemplate pretty={pretty} />
-      <a className={utilStyles.noPrint} href='/assets/Roberto-Andrade_Creative-Technologist.pdf'>Download PDF</a>
+      <div className={utilStyles.noPrint}>
+        <button onClick={() => setPretty(!pretty)}>View {pretty ? 'CV' : 'Resume'}</button>
+
+        or
+        <a href={`/assets/Andrade-Creative_Technologist${pretty ? '' : ' (machine)'}.pdf`}>Download PDF</a>
+      </div>
     </Layout>
   )
 }
