5ac3c36ef3447f6acbcad295b084befd2f70caa2
Robert Andrade <srandradejr@gmail.com>
2022-01-02 04:55:52 +0000

    remove jspdf

--- pages/resume.js
+++ pages/resume.js
@@ -2,21 +2,14 @@ import Layout from '../components/layout';
 import { useState } from 'react';
 import ResumeTemplate from '../components/resumeTemplate';
 import utilStyles from '../styles/utils.module.scss';
-import { downloadResume } from '../lib/pdf';
 
 export default function Resume() {
   const [pretty, setPretty] = useState(false);
-
-  const togglePretty = () => {
-    console.log('toggling pretty', pretty);
-    setPretty(!pretty);
-  }
 
   return (
     <Layout>
       <button className={utilStyles.noPrint} onClick={() => setPretty(!pretty)}>toggle pretty</button>
       <ResumeTemplate pretty={pretty} />
-      <button className={utilStyles.noPrint} onClick={downloadResume}>create pdf</button><br />
       <a className={utilStyles.noPrint} href='/assets/Roberto-Andrade_Creative-Technologist.pdf'>Download PDF</a>
     </Layout>
   )
