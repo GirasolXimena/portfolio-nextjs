f6cb8897f9e28a1a5416ae4146ccd580427a3a3c
Robert Andrade <srandradejr@gmail.com>
2022-01-04 04:29:54 +0000

    arrow on the hero

--- components/drawArrow.js
+++ components/drawArrow.js
@@ -1,25 +1,25 @@
 
 
 
-function canvas_arrow(context, fromx, fromy, tox, toy) {
-  var headlen = 10; // length of head in pixels
+function canvas_arrow(context, fromx, fromy, tox, toy, headlen = 50) {
   var dx = tox - fromx;
   var dy = toy - fromy;
   var angle = Math.atan2(dy, dx);
+  context.lineWidth = 20
   context.moveTo(fromx, fromy);
   context.lineTo(tox, toy);
-  context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
   context.moveTo(tox, toy);
+  context.lineTo(tox - headlen * Math.cos(angle - Math.PI / 6), toy - headlen * Math.sin(angle - Math.PI / 6));
   context.lineTo(tox - headlen * Math.cos(angle + Math.PI / 6), toy - headlen * Math.sin(angle + Math.PI / 6));
+  context.lineTo(tox, toy);
+  context.closePath()
+  context.fill()
 }
 
 export default function Arrow(ctx) {
 
   console.log('ctx', ctx)
   ctx.beginPath();
-  canvas_arrow(ctx, 10, 30, 200, 150);
-  canvas_arrow(ctx, 100, 200, 400, 50);
-  canvas_arrow(ctx, 200, 30, 10, 150);
-  canvas_arrow(ctx, 400, 200, 100, 50);
+  canvas_arrow(ctx, 25, 75, 250, 75);
   ctx.stroke();
 }
--- pages/index.js
+++ pages/index.js
@@ -12,13 +12,7 @@ export default function Home() {
       </Head>
       <main className={utilStyles.headingMd}>
         <Hero />
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </main>
-      <Link href="/resume"><a>View Resume</a></Link>
     </div>
   )
 }
