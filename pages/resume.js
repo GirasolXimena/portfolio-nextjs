4d8393aff15473dbcf32d384e4e15d54cfeb4f8c
Robert Andrade <srandradejr@gmail.com>
2022-01-13 09:45:19 +0000

    Show pdf in iframe to combat spam and SEO

--- pages/resume.js
+++ pages/resume.js
@@ -5,15 +5,19 @@ import utilStyles from '../styles/utils.module.scss';
 
 export default function Resume() {
   const [pretty, setPretty] = useState(true);
+  const resumeUrl = `/assets/Andrade-Creative_Technologist${pretty ? '' : ' (machine)'}.pdf`
 
   return (
     <Layout>
-      <ResumeTemplate pretty={pretty} />
+      <iframe src={resumeUrl} width="900px" height="1200px">
+      </iframe>
+
+      {/* <ResumeTemplate pretty={pretty} /> */}
       <div className={utilStyles.noPrint}>
         <button onClick={() => setPretty(!pretty)}>View {pretty ? 'CV' : 'Resume'}</button>
 
         or
-        <a href={`/assets/Andrade-Creative_Technologist${pretty ? '' : ' (machine)'}.pdf`}>Download PDF</a>
+        <a href={resumeUrl}>Download PDF</a>
       </div>
     </Layout>
   )
