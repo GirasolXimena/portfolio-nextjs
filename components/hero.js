78ab34209a5ea20e14eab8ae1dade6ab98c87212
Roberto Andrade <srandradejr@gmail.com>
2022-02-05 20:16:47 +0000

    anim dependent on box

--- components/hero.js
+++ components/hero.js
@@ -3,60 +3,38 @@ import Navbar from './navbar'
 import { useEffect, useState } from 'react'
 
 export default function Hero() {
-  const [mousePosition, setMousePosition] = useState({
-    H1: {
-      x: 1,
-      y: 1
-    },
-    H2: {
-      x: 1,
-      y: 1
-    }
-  })
+  const [mousePosition, setMousePosition] = useState({ x: 1, y: 1 })
 
   const setMouse = (e) => {
-    let { x, y, target } = e
-    x = x - target.offsetLeft + 1
-    y = y - target.offsetTop + 1
-    const halfWidth = target.offsetWidth / 2
-    const halfHeight = target.offsetHeight / 2
+    const offset = document.getElementById('hero').getBoundingClientRect()
+    const { left, top, width, height } = offset
+    const { clientX, clientY } = e
+    const x = clientX - left
+    const y = clientY - top
+    const halfWidth = width / 2
+    const halfHeight = height / 2
     const xPos = x - halfWidth
     const yPos = y - halfHeight
-    const { tagName } = target
-    const { height, width } = target.getBoundingClientRect()
-    console.log('set mouse', xPos, yPos)
-    setMousePosition(state => ({
-      ...state,
-      [tagName]: {
-        x: xPos / width * 2,
-        y: yPos / height * 2
-      }
-    }))
+    setMousePosition({
+      x: xPos / width * 2,
+      y: yPos / height * 2
+    })
   }
 
-  const resetMouse = (e) => {
-    const { tagName } = e.target
-    setMousePosition(state => ({
-      ...state,
-      [tagName]: {
-        x: 1,
-        y: 1
-      }
-    }))
+  const resetMouse = (_e) => {
+    setMousePosition({
+      x: 1,
+      y: 1
+    })
   }
   useEffect(() => {
-    const h1 = document.querySelector('#hero h1')
-    const h2 = document.querySelector('#hero h2')
-    h1.addEventListener('mousemove', setMouse)
-    h1.addEventListener('mouseleave', resetMouse)
-    h2.addEventListener('mousemove', setMouse)
-    h2.addEventListener('mouseleave', resetMouse)
+    const hero = document.getElementById('hero')
+    hero.addEventListener('mousemove', setMouse)
+    hero.addEventListener('mouseleave', resetMouse)
 
     return () => {
-      h1.removeEventListener('mousemove', setMouse)
-      h1.removeEventListener('mouseleave', resetMouse)
-      h2.removeEventListener('mousemove', setMouse)
-      h2.removeEventListener('mouseleave', resetMouse)
+      hero.removeEventListener('mousemove', setMouse)
+      hero.removeEventListener('mouseleave', resetMouse)
     };
   }, []);
 
@@ -65,9 +43,10 @@ export default function Hero() {
       <h1
         style={{
           textShadow: `
-            0 ${mousePosition.H1.y * -5}px cyan,
-            ${mousePosition.H1.x * -5}px ${mousePosition.H1.y * -5}px magenta,
-            ${mousePosition.H1.y * 5}px ${mousePosition.H1.y * -5}px yellow`
+            ${mousePosition.y * 5}px ${mousePosition.y * -5}px yellow,
+            0 ${mousePosition.y * -5}px cyan,
+            ${mousePosition.x * -5}px ${mousePosition.y * -5}px magenta
+            `
         }}
         className={styles.cmyk}
       >
@@ -77,16 +56,17 @@ export default function Hero() {
       <h2
         style={{
           textShadow: `
-            0 ${mousePosition.H2.y * -5}px cyan,
-            ${mousePosition.H2.x * -5}px ${mousePosition.H2.y * -5}px magenta,
-            ${mousePosition.H2.y * 5}px ${mousePosition.H2.y * -5}px yellow`
+            ${mousePosition.y * 5}px ${mousePosition.y * -5}px yellow,
+            0 ${mousePosition.y * -5}px cyan,
+            ${mousePosition.x * -5}px ${mousePosition.y * -5}px magenta
+            `
         }}
         className={styles.cmyk}
       >
         Creative Technologist
       </h2>
       <Navbar home />
-      <span style={{ position: 'absolute' }}>
+      <span style={{ position: 'fixed', top: 0, left: 0 }}>
         {JSON.stringify(mousePosition)}
       </span>
     </div>
