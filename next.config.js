c0b3be67052c4003454db50605be65d8bc8b95f8
Roberto Andrade <srandradejr@gmail.com>
2022-02-05 21:43:45 +0000

    always redirect home

---
+++ next.config.js
@@ -0,0 +1,16 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path',
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
+}
+
+module.exports = nextConfig
