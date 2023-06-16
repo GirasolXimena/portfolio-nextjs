028cdd7d9fc30557f8d6b9529e4b58a53fcb9673
Roberto Andrade <robert.andrade@zinnia.com>
2023-06-16 16:55:37 +0000

    update Link

--- pages/labs/index.js
+++ pages/labs/index.js
@@ -24,7 +24,7 @@ export default function PostsLanding({ allPostsData }) {
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
-                <a>{title}</a>
+                {title}
               </Link>
               <br />
               <small className={utilStyles.lightText}>
