ec551dd7a8c418294f5d17827e367e3aa758a440
Roberto Andrade <robert.andrade@zinnia.com>
2023-07-19 21:21:27 +0000

    map links

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -1,13 +1,29 @@
 import Link from "next/link";
+const links = [
+  {
+    href: '/labs/eloquent-js',
+    label: 'Eloquent JS Exercises',
+  },
+  {
+    href: 'https://codepen.io/abstract_code',
+    label: 'Codepen',
+  },
+]
 
 export default async function LabsIndexPage() {
   return (
     <>
-      <h1>hello</h1>
+      <h1>Labs</h1>
       <ul>
-        <li>
-          <Link href="/labs/eloquent-js">Eloquent JS Exercises</Link>
-        </li>
+        {
+          links.map(({ href, label }) => (
+            <li key={`${href}${label}`}>
+              <Link href={href}>
+                {label}
+              </Link>
+            </li>
+          ))
+        }
       </ul>
     </>
   )
