80fc6b3f20e4630367301660a7499e03a792be54
Roberto Andrade <robert.andrade@zinnia.com>
2023-07-19 21:02:56 +0000

    update labs url scheme

--- app/labs/page.tsx
+++ app/labs/page.tsx
@@ -1,10 +1,14 @@
-// Import your Client Component
-import ListExercises from './list-exercises'
-import { getFolderContent } from '../../api/github'
- 
+import Link from "next/link";
+
 export default async function LabsIndexPage() {
-  // Fetch data directly in a Server Component
-  const eloquentExercises = await getFolderContent('RobertAndradeJr', 'eloquent-js-exercises', 'src')
-  // Forward fetched data to your Client Component
-  return <ListExercises title={'Eloquent JS Exercises'} exercises={eloquentExercises} />
+  return (
+    <>
+      <h1>hello</h1>
+      <ul>
+        <li>
+          <Link href="/labs/eloquent-js">Eloquent JS Exercises</Link>
+        </li>
+      </ul>
+    </>
+  )
 }
