409170615c9152d871181b4af8b6446de90f39bd
Roberto Andrade <robert.andrade@zinnia.com>
2023-07-20 17:04:29 +0000

    cleanup fetching

--- app/labs/eloquent-js/[slug]/page.tsx
+++ app/labs/eloquent-js/[slug]/page.tsx
@@ -1,13 +1,11 @@
-import { getContent } from "../../../../api/github"
+import { getEloquentFile } from "../../../../api/github"
 import Exercise from "./exercise"
 
 export default async function EloquentJSPage({ params }: { params: { slug: string } }) {
-  const {name, type, html_url, encoding, content } = await getContent('RobertAndradeJr', 'eloquent-js-exercises', params.slug)
-  const res = await getContent('RobertAndradeJr', 'eloquent-js-exercises', params.slug)
+  const {name, type, html_url, encoding, content } = await getEloquentFile(params.slug)
 
   return (
     <div>
-      <h1>{res.path}</h1>
       {type === 'file' && <Exercise title={name} github_link={html_url} encoding={encoding} content={content} />}
     </div>
   )
