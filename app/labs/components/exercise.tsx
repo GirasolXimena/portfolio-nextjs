29a7a89a0a2de9a7336b504b9f3b059dcf6d6d3c
Roberto Andrade <robert.andrade@zinnia.com>
2023-07-23 20:10:23 +0000

    restyle exercise detail page

--- app/labs/components/exercise.tsx
+++ app/labs/components/exercise.tsx
@@ -1,24 +1,41 @@
 'use client'
- import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/themes/prism-tomorrow.css";
+import styles from './exercise.module.css'
+import Link from 'next/link'
 // This is a Client Component. It receives data as props and
 // has access to state and effects just like Page components
 // in the `pages` directory.
-function ExerciseDetailPage({ title, content, github_link, encoding, type }) {
-  if(type !== 'file') {
+function ExerciseDetailPage({ title, content, github_link, type, subtitle, prismName }) {
+  const theme = 'tomorrow-night-eighties'
+  if (type !== 'file') {
     return (
       <div>
         <h1>Not a file</h1>
       </div>
     )
   }
+  const formattedTitle = title.split('.')[0].split('-').map((word) => word[0].toUpperCase() + word.slice(1)).join(' ')
   return (
-    <div>
-      <h1>{title}</h1>
-      <pre style={{ backgroundColor: 'black', maxWidth: '800px', overflowX: 'auto'}}>
-        <code className="language-js" dangerouslySetInnerHTML={{__html: content}} />
-      </pre>
-      <a href={github_link}>View on Github!</a>
-    </div>
+    <article className={styles.exercise}>
+      <h1 className={styles.title}>{formattedTitle}</h1>
+      
+      <h2 className={styles.subtitle}>
+        <Link href={`../`}>{subtitle}</Link>
+      </h2>
+      <figure className={styles.content}>
+        <pre className={`${styles.code} ${styles[theme]}`}>
+          <code className={`language-${prismName}`} dangerouslySetInnerHTML={{ __html: content }} />
+        </pre>
+        <figcaption className={styles['see-more']}>
+            <span className="language">
+              {prismName}
+            </span>
+            <span className="link">
+              View on <a href={github_link}>Github</a>
+            </span>
+        </figcaption>
+      </figure>
+    </article>
   )
 }
 
