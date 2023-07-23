521efdd8d624e54da6a614b25dbc97ad69ffd5bd
Roberto Andrade <robert.andrade@zinnia.com>
2023-07-23 20:10:41 +0000

    style updates to labs page

--- lib/util/string.ts
+++ lib/util/string.ts
@@ -1,1 +1,3 @@
 export const normalizePath = (path: string, baseDir: string) => path.split('/').filter(segment => segment !== '.' && segment !== '..' && segment !== baseDir).join('/')
+
+export const getExtension = (path: string) => path.split('.').pop() || ''
