e89bd1b0138fbd36a26bb097716006d5d684510d
Roberto Andrade <robert.andrade@zinnia.com>
2023-07-31 05:20:14 +0000

    mouse coord update to not rerender

--- hooks/useMouseCoordinates.ts
+++ hooks/useMouseCoordinates.ts
@@ -1,35 +1,46 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react";
 
-function useMouseCoordinates(cartesian = false, prefersReducedMotion = false) {
-  const [mouseCoordinates, setMouseCoordinates] = useState({ x: 0, y: 0 })
+function useMouseCoordinates(cartesian = false, prefersReducedMotion = false, causesRerender = true) {
+  const mouseCoordinatesRef = useRef({ x: 0, y: 0 });
+  const [mouseCoordinatesState, setMouseCoordinatesState] = useState({ x: 0, y: 0 });
+
   useEffect(() => {
     console.log('setting up mouse coordinates')
-    const handleMouseMove = (event: MouseEvent) => {
+
+    const handleMouseMove = (event) => {
       console.log('mouse move')
+
+      let newCoordinates;
+
       if (cartesian) {
-        setMouseCoordinates({
+        newCoordinates = {
           x: (event.clientX - (window.innerWidth / 2)) / (window.innerWidth / 2),
           y: (event.clientY - (window.innerHeight / 2)) / (window.innerHeight / 2)
-        })
+        };
       } else {
-        setMouseCoordinates({
+        newCoordinates = {
           x: event.clientX,
           y: event.clientY
-        })
+        };
       }
-    }
+
+      mouseCoordinatesRef.current = newCoordinates;
+      if (causesRerender) {
+        setMouseCoordinatesState(newCoordinates);
+      }
+    };
+
     if (!prefersReducedMotion) {
-      document.addEventListener("mousemove", handleMouseMove)
+      document.addEventListener("mousemove", handleMouseMove);
     }
     return () => {
       if (!prefersReducedMotion) {
-        document.removeEventListener("mousemove", handleMouseMove)
+        document.removeEventListener("mousemove", handleMouseMove);
       }
-    }
-  }, [prefersReducedMotion, cartesian])
+    };
+  }, [prefersReducedMotion, cartesian, causesRerender]);
 
-  return mouseCoordinates
+  return causesRerender ? mouseCoordinatesState : mouseCoordinatesRef.current;
 }
 
-
-export default useMouseCoordinates
+export default useMouseCoordinates;
