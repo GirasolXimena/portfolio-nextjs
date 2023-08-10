ae1f6f310f666302a1ba81a1ed2ff75817b04b43
Roberto Andrade <robert.andrade@zinnia.com>
2023-08-10 23:27:49 +0000

    prevent double clicks

--- components/palette-switcher.tsx
+++ components/palette-switcher.tsx
@@ -8,6 +8,7 @@ function PaletteSwitcher({ currentPalette, setPalette, segment }) {
   const paletteKeys = Object.keys(palettes)
   const nextPaletteIndex = paletteKeys.indexOf(currentPalette) + 1
   const nextPalette = paletteKeys[nextPaletteIndex] || paletteKeys[0]
+  const isTransitioning = useRef(false)
 
   useEffect(() => {
     if (!paletteSwitcherRef.current) return
@@ -17,16 +18,21 @@ function PaletteSwitcher({ currentPalette, setPalette, segment }) {
   }, [nextPalette])
 
   const handleClick = () => {
+    // prevent double clicks
+    if (isTransitioning.current) return
+
     const bodyElem = document.body;
 
     const handleTransitionEnd = (event) => {
+    
       if (event.propertyName === 'opacity') {
         setPalette(nextPalette);
         bodyElem.removeEventListener('transitionend', handleTransitionEnd);
         bodyElem.classList.remove('transitioning');
+        isTransitioning.current = false
       }
     };
-
+    isTransitioning.current = true
     bodyElem.classList.add('transitioning');
     bodyElem.addEventListener('transitionend', handleTransitionEnd);
   }
@@ -34,6 +40,7 @@ function PaletteSwitcher({ currentPalette, setPalette, segment }) {
   return (
     <div className={`${styles.container} ${styles[segment]}`}>
       <button
+        disabled={isTransitioning.current}
         ref={paletteSwitcherRef}
         onClick={handleClick}
         aria-label="change palette"
