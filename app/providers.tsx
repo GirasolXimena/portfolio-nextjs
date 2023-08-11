ab8d139add2f28873d64e9a6b4d63eab70da0f02
Roberto Andrade <robert.andrade@zinnia.com>
2023-08-11 21:17:36 +0000

    move shadow text to providers

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,15 +1,50 @@
 'use client'
 
 import { ThemeProvider } from "next-themes"
-import ShadowText from "../components/shadow-text"
+import { useEffect } from "react";
+import utilities from 'lib/util';
+const { setCustomProperties, toCartesianCoords } = utilities
+
+const handleInput = (event: MouseEvent | TouchEvent) => {
+  let x: number;
+  let y: number;
+
+  if (event instanceof TouchEvent && event.touches.length > 0) {
+    x = event.touches[0].clientX;
+    y = event.touches[0].clientY;
+  } else if (event instanceof MouseEvent) {
+    x = event.clientX;
+    y = event.clientY;
+  } else {
+    return;  // Early return if neither condition met
+  }
+
+  const coords = toCartesianCoords({ x, y });
+  setCustomProperties({
+    'mouse-x': `${coords.x}`,
+    'mouse-y': `${coords.y}`,
+  });
+}
+
+
 
 export function Providers({ children }) {
+
+  useEffect(() => {
+    const { body } = document;
+
+    body.addEventListener('mousemove', handleInput);
+    body.addEventListener('touchmove', handleInput);
+
+    return () => {
+      body.removeEventListener('mousemove', handleInput);
+      body.removeEventListener('touchmove', handleInput);
+    };
+  }, []);
+
   return (
     <ThemeProvider>
-      <ShadowText textClass={'shadow'}>
-        {children}
-
-      </ShadowText>
+      {children}
     </ThemeProvider>
   )
 }
