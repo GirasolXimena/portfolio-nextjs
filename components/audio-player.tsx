b1eadfe4b6e438f0d84242f33e08b986ceb83314
Roberto Andrade <robert.andrade@zinnia.com>
2023-08-21 04:59:27 +0000

    never touch hero again

--- components/audio-player.tsx
+++ components/audio-player.tsx
@@ -3,13 +3,52 @@ import { useEffect, useRef, useState } from "react";
 import utilities from "../lib/util";
 import SoundIcon from "./mute-icon";
 import styles from "../styles/audio-player.module.scss"
+import { useAnimationFrame } from "framer-motion";
+import usePrefersReducedMotion from "@/hooks/usePreferesReducedMotion";
+
+const noAudioAnimation = (time) => {
+  const now = time / 1000
+
+  // Normalize sin and cosine values to [0, 1] range
+  const primaryAmp = (Math.sin(2 * Math.PI * now / 15) + 1) / 2;
+  const secondaryAmp = (Math.sin(2 * Math.PI * now / 30) + 1) / 2;
+  const tertiaryAmp = (Math.sin(2 * Math.PI * now / 60) + 1) / 2;
+
+  // Scale and translate to desired range, for example, [1, 1.5]
+  const primaryValue = primaryAmp;
+  const secondaryValue = secondaryAmp;
+  const tertiaryValue = tertiaryAmp;
+  document.documentElement.style.setProperty('--amp-primary', String(primaryValue));
+  document.documentElement.style.setProperty('--amp-secondary', String(secondaryValue));
+  document.documentElement.style.setProperty('--amp-tertiary', String(tertiaryValue));
+}
+const audioAnimation = (audioData) => {
+  // create a new array of 32 bit floating point numbers
+  if (!audioData.current) return
+  let data = new Float32Array(audioData.current.frequencyBinCount)
+  // draw the audio data
+  const draw = (data: number) => {
+    const val = 0.33 + data / 3
+    utilities.setCustomProperties({
+      '--amp-primary': String(val),
+      '--amp-secondary': String(val),
+      '--amp-tertiary': String(val),
+    })
+  }
+  if (!audioData.current) return
+  audioData.current.getFloatTimeDomainData(data)
+  let sumQuares = 0.0
+  for (const ampliltude of data) {
+    sumQuares += ampliltude * ampliltude
+  }
+  const amp = Math.sqrt(sumQuares / data.length)
+  draw(amp)
+}
 
 function AudioPlayer({ musicType, segment }: { musicType: string | undefined; segment: string }) {
   const audioElement = useRef<HTMLAudioElement>(null);
   const audioData = useRef<AnalyserNode | null>(null);
-  const [animation, setAnimation] = useState<number>(0);
-  const playing = useRef<boolean>(false);
-  const now = useRef<number>(0);
+  const [playing, setPlaying] = useState<boolean>(false);
   const audioContext = useRef<AudioContext | null>(null);
 
   const createAudioContext = () => {
@@ -24,10 +63,7 @@ function AudioPlayer({ musicType, segment }: { musicType: string | undefined; se
   }
 
   const startPlaying = () => {
-    cancelAnimationFrame(animation)
-    setAnimation(0)
-    if(playing.current) return
-    if (!musicType) return
+    if (playing || !musicType) return
     // if there is no audio element or audio context, create them
     if (!audioContext.current) {
       createAudioContext()
@@ -38,73 +74,36 @@ function AudioPlayer({ musicType, segment }: { musicType: string | undefined; se
     audioElement.current.src = musicType
     // 
     audioElement.current.play()
-    playing.current = true
-    // create a new array of 32 bit floating point numbers
-    let data = new Float32Array(audioData.current.frequencyBinCount)
-    // draw the audio data
-    const draw = (data: number) => {
-      const val = 1.5 + data / 10
-      utilities.setCustomProperties({
-        '--amp-primary': String(val),
-        '--amp-secondary': String(val),
-        '--amp-tertiary': String(val),
-      })
-    }
-
-    const loop = () => {
-      if (!audioData.current) return
-      audioData.current.getFloatTimeDomainData(data)
-      let sumQuares = 0.0
-      for (const ampliltude of data) {
-        sumQuares += ampliltude * ampliltude
-      }
-      const amp = Math.sqrt(sumQuares / data.length)
-      draw(amp)
-      const id = requestAnimationFrame(loop)
-      setAnimation(id)
-    }
-    const id = requestAnimationFrame(loop)
-    setAnimation(id);
-
+    setPlaying(true)
   }
 
   const stopPlaying = () => {
     if (!audioElement.current) return
     audioElement.current.pause()
-    cancelAnimationFrame(animation)
-    setAnimation(0);
-    playing.current = false
-
-    function updateAmpValues() {
-      now.current += 1 / 90
-      
-      // Normalize sin and cosine values to [0, 1] range
-      const primaryAmp = (Math.sin(2 * Math.PI * now.current / 15) + 1) / 2;
-      const secondaryAmp = (Math.sin(2 * Math.PI * now.current / 30) + 1) / 2;
-      const tertiaryAmp = (Math.sin(2 * Math.PI * now.current / 60) + 1) / 2;
-  
-      // Scale and translate to desired range, for example, [1, 1.5]
-      const primaryValue = 1 + 0.5 * primaryAmp;
-      const secondaryValue = 1 + 0.5 * secondaryAmp;
-      const tertiaryValue = 1 + 0.5 * tertiaryAmp;
-      document.documentElement.style.setProperty('--amp-primary', String(primaryValue));
-      document.documentElement.style.setProperty('--amp-secondary', String(secondaryValue));
-      document.documentElement.style.setProperty('--amp-tertiary', String(tertiaryValue));
+    setPlaying(false)
   }
 
-    const loop = () => {
-      updateAmpValues()
-      const id = requestAnimationFrame(loop)
-      setAnimation(id)
+  const togglePlaying = () => {
+    if (playing) {
+      stopPlaying()
+    } else if (!playing) {
+      startPlaying()
     }
-    const id = requestAnimationFrame(loop)
-    setAnimation(id)
   }
 
+
+  const shouldAnimate = !usePrefersReducedMotion()
+  useAnimationFrame((time) => {
+    if (!shouldAnimate) return
+    if(playing) {
+      audioAnimation(audioData)
+    } else {
+      noAudioAnimation(time)
+    }
+  })
 
   useEffect(() => {
     stopPlaying()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [musicType])
 
   return (
@@ -112,12 +111,12 @@ function AudioPlayer({ musicType, segment }: { musicType: string | undefined; se
       {
         !!musicType && (
           <button
-            className={`${styles.button} ${animation && 'playing'}`}
-            onClick={playing.current ? stopPlaying : startPlaying}
-            data-label={`${animation ? 'mute' : 'audio'}`}
-            aria-label={`${animation ? 'stop' : 'play'} audio`}
+            className={`${styles.button}`}
+            onClick={togglePlaying}
+            data-label={`${playing ? 'mute' : 'audio'}`}
+            aria-label={`${playing ? 'stop' : 'play'} audio`}
           >
-            <SoundIcon size="36" playing={playing.current} />
+            <SoundIcon size="36" playing={playing} />
           </button>
         )
       }
