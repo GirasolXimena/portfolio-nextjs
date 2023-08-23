0bd499292433293b94b5258224d503a2c9adeb2e
Roberto Andrade <robert.andrade@zinnia.com>
2023-08-23 02:20:38 +0000

    works so good

--- components/background-animation.tsx
+++ components/background-animation.tsx
@@ -1,75 +1,81 @@
-import usePrefersReducedMotion from "@/hooks/usePreferesReducedMotion";
-import utilities from "@/utils/index";
-import { useAudioContext } from "app/providers"
-import { useAnimationFrame } from "framer-motion";
-import { useCallback } from "react";
+import {
+  motion,
+  useAnimationFrame,
+
+} from "framer-motion";
+import { useState } from "react";
+
+const ORBIT_RADIUS = 20; // Control how far the gradient moves from the center
+const customComputeX = (i) => 50 + (ORBIT_RADIUS * Math.sin(i + (Math.PI / 2 * i))); // Add phase shifts for variation
+const customComputeY = (i) => 50 + (ORBIT_RADIUS * Math.cos(i + (Math.PI / 3 * i))); // Add phase shifts for variation
+
+
+function Gradient({ index, time, color }) {
+  const x = customComputeX(time + index);
+  const y = customComputeY(time + index);
+  console.log({ x, y })
+  return (
+    <motion.div
+      transition={{ duration: 0.05 }}
+      style={{
+        backgroundImage: `radial-gradient(farthest-corner at ${x}% ${y}%, var(--${color}), transparent)`,
+        // display: 'none',
+        zIndex: -5,
+        originX: 0,
+        originY: 0,
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+      }}
+    />
+  );
+
+}
 
 function BackgroundAnimation({ children, className }) {
-  const { playing, audioData } = useAudioContext()
-  // do this next!
-  // const { currentPalette } = usePaletteContext()
+  const [time, setTime] = useState(0);
 
-  const audioAnimation = useCallback((audioData) => {
-    // create a new array of 32 bit floating point numbers
-    if (!audioData.current) return
-    let data = new Float32Array(audioData.current.frequencyBinCount)
-    // draw the audio data
-    const draw = (data: number) => {
-      const val = 0.33 + data / 3
-      utilities.setCustomProperties({
-        '--amp-primary': String(val),
-        '--amp-secondary': String(val),
-        '--amp-tertiary': String(val),
-      })
-    }
-    if (!audioData.current) return
-    audioData.current.getFloatTimeDomainData(data)
-    let sumQuares = 0.0
-    for (const ampliltude of data) {
-      sumQuares += ampliltude * ampliltude
-    }
-    const amp = Math.sqrt(sumQuares / data.length)
-    draw(amp)
-  }, [])
-
-  const noAudioAnimation = useCallback((time: number) => {
-    const now = time / 1000
-
-    // Normalize sin and cosine values to [0, 1] range
-    const primaryAmp = (Math.sin(2 * Math.PI * now / 15) + 1) / 2;
-    const secondaryAmp = (Math.sin(2 * Math.PI * now / 30) + 1) / 2;
-    const tertiaryAmp = (Math.sin(2 * Math.PI * now / 60) + 1) / 2;
-
-    // Scale and translate to desired range, for example, [1, 1.5]
-    const primaryValue = primaryAmp / 2;
-    const secondaryValue = secondaryAmp / 2;
-    const tertiaryValue = tertiaryAmp / 2;
-    utilities.setCustomProperties({
-      '--amp-primary': String(primaryValue),
-      '--amp-secondary': String(secondaryValue),
-      '--amp-tertiary': String(tertiaryValue),
-    })
-  }, [])
-
-  const shouldAnimate = !usePrefersReducedMotion()
   useAnimationFrame((time) => {
-    if (!shouldAnimate) return
-    if (playing) {
-      audioAnimation(audioData)
-    } else {
-      // noAudioAnimation(time)
-    }
+    const adjustedTime = time / 3000;
+    setTime(adjustedTime);
   })
-  return (
-    <div
-      className={className}
-      style={{
-        // backgroundColor: 'black'
 
-      }}
-    >
+  return (
+    <>
+      <motion.div
+        className={className}
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          zIndex: -1,
+          width: '100vw',
+          height: '100vh',
+          backgroundImage: 'linear-gradient(var(--background), var(--background)), var(--noise-1)',
+          backgroundSize: 'cover',
+          mixBlendMode: 'soft-light',
+          opacity: 1,
+        }}
+      />
+      <Gradient
+        index={0}
+        time={time}
+        color="primary"
+      />
+      <Gradient
+        index={1}
+        time={time}
+        color="secondary"
+      />
+      <Gradient
+        index={2}
+        time={time}
+        color="tertiary"
+      />
       {children}
-    </div>
+    </>
   )
 }
 
