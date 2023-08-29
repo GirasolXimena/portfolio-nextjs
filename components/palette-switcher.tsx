d9483d46ca74f5ca6a7a7f367cbcbd3aec57ec4a
Roberto Andrade <robert.andrade@zinnia.com>
2023-08-29 02:03:31 +0000

    color transitions are working!!!!

--- components/palette-switcher.tsx
+++ components/palette-switcher.tsx
@@ -7,43 +7,18 @@ import usePaletteContext from "hooks/usePaletteContext";
 function PaletteSwitcher({ segment }) {
   const paletteSwitcherRef = useRef<HTMLButtonElement>(null);
   const isTransitioning = useRef(false)
-  const { palette, setPalette, nextPalette } = usePaletteContext()
+  const { setPalette, nextPalette } = usePaletteContext()
 
   useEffect(() => {
     if (!paletteSwitcherRef.current) return
-    const properties = palettes[nextPalette].properties
+    const properties = nextPalette.palette.properties
     const element = paletteSwitcherRef.current
     utilities.setCustomProperties(properties, element)
   }, [nextPalette])
 
-  useEffect(() => {
-    const storedPalette = localStorage.getItem('user-palette') || 'default';
-    setPalette(storedPalette)
-  }, [setPalette])
-
-  useEffect(() => {
-    localStorage.setItem('user-palette', palette)
-    utilities.setCustomProperties(palettes[palette].properties)
-  }, [palette])
-
   const handleClick = () => {
-    // prevent double clicks
-    if (isTransitioning.current) return
 
-    const bodyElem = document.body;
-
-    const handleTransitionEnd = (event) => {
-    
-      if (event.propertyName === 'opacity' && event.srcElement.nodeName === 'BODY') {
-        setPalette(nextPalette);
-        bodyElem.removeEventListener('transitionend', handleTransitionEnd);
-        bodyElem.classList.remove('transitioning');
-        isTransitioning.current = false
-      }
-    };
-    isTransitioning.current = true
-    bodyElem.classList.add('transitioning');
-    bodyElem.addEventListener('transitionend', handleTransitionEnd);
+    setPalette(nextPalette.key);
   }
 
   return (
--- providers/palette-context.tsx
+++ providers/palette-context.tsx
@@ -1,12 +1,15 @@
+import { animateProperties, setCustomProperties } from "lib/util";
 import {
   Dispatch,
   ReactNode,
   SetStateAction,
   createContext,
-  useState
+  useRef,
+  useMemo
 } from "react";
 import palettes from "styles/palettes";
 import { Palette } from "types";
+import { useEffectOnce, useLocalStorage, useUpdateEffect } from "usehooks-ts";
 
 type PaletteContextProviderProps = {
   children: ReactNode
@@ -14,31 +17,53 @@ type PaletteContextProviderProps = {
 
 type PaletteContextType = {
   palette: string,
-  nextPalette: string,
-  currentPalette: Palette
+  nextPalette: { palette: Palette, key: string },
+  currentPalette: Palette,
   setPalette: Dispatch<SetStateAction<string>>
 }
 
 export const PaletteContext = createContext<PaletteContextType | undefined>(undefined);
 
 const PaletteContextProvider = ({ children }: PaletteContextProviderProps) => {
-  const [palette, setPalette] = useState('default');
-  const paletteKeys = Object.keys(palettes)
-  const nextPaletteIndex = paletteKeys.indexOf(palette) + 1
-  const nextPalette = paletteKeys[nextPaletteIndex] || paletteKeys[0]
-  const currentPalette = palettes[palette]
+  const [palette, setPalette] = useLocalStorage('user-palette', 'default');
+  const paletteKeys = Object.keys(palettes);
 
+  const nextPalette = useMemo(() => {
+    const nextPaletteIndex = (paletteKeys.indexOf(palette) + 1) % paletteKeys.length;
+    const nextPaletteKey = paletteKeys[nextPaletteIndex];
+    return { palette: palettes[nextPaletteKey], key: nextPaletteKey };
+  }, [palette, paletteKeys]);
+
+  const currentPalette = palettes[palette];
+  const prevPalette = useRef<string | undefined>(palette);
+
+  useEffectOnce(() => {
+    setCustomProperties(currentPalette.properties);
+  });
+
+  useUpdateEffect(() => {
+    if (!prevPalette.current || prevPalette.current === palette) return;
+
+    const targetProperties = currentPalette.properties;
+    const sourceProperties = palettes[prevPalette.current].properties;
+
+    // Transitioning between the palette properties
+    for (let propertyKey in targetProperties) {
+      animateProperties(
+        sourceProperties[propertyKey],
+        targetProperties[propertyKey],
+        propertyKey
+      );
+    }
+
+    prevPalette.current = palette;
+  }, [palette, currentPalette.properties]);
 
   return (
-    <PaletteContext.Provider value={{
-      palette,
-      currentPalette,
-      nextPalette,
-      setPalette
-    }}>
+    <PaletteContext.Provider value={{ palette, currentPalette, nextPalette, setPalette }}>
       {children}
     </PaletteContext.Provider>
-  )
+  );
 }
 
 export default PaletteContextProvider;
