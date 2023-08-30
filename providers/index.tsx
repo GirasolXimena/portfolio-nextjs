53c12a232cda7a06b13d7062d0bc57d88d491a9b
Roberto Andrade <robert.andrade@zinnia.com>
2023-08-30 18:24:32 +0000

    restore useMouseInput

--- providers/index.tsx
+++ providers/index.tsx
@@ -1,10 +1,12 @@
 'use client'
+import { useMouseInput } from 'hooks/useMouseInput';
 import { usePathname } from 'next/navigation';
 import CompositeProvider from "providers/composite-context";
 import TransitionContext from 'providers/transition-context';
 
 export function Providers({ children }) {
   const pathname = usePathname();
+  useMouseInput();
 
   return (
     <TransitionContext transitionKey={pathname}>
