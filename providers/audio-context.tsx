2a34167df5a9db4e1e7432e02220a2cd6df8cbf4
Roberto Andrade <robert.andrade@zinnia.com>
2023-08-31 20:47:05 +0000

    try css animation

--- providers/audio-context.tsx
+++ providers/audio-context.tsx
@@ -46,14 +46,8 @@ const AudioContextProvider = ({ children, audioControlHook = useAudioControl }: 
     startPlaying,
     stopPlaying
   } = audioControlHook()
-  const primaryValue = useMotionValue(0)
-  const secondaryValue = useMotionValue(0)
-  const tertiaryValue = useMotionValue(0)
   const audioLevel = useMotionValue(0)
 
-  const normalizedPrimaryLevel = useTransform(primaryValue, [0, 1], [0, 1])
-  const normalizedSecondaryLevel = useTransform(secondaryValue, [0, 1], [0, 1])
-  const normalizedTertiaryLevel = useTransform(tertiaryValue, [0, 1], [0, 1])
   const normalizedAudioLevel = useTransform(audioLevel, [0, 0.5], [0, 1])
 
   const audioAnimation = useCallback((audioData: AudioDataType) => {
@@ -79,41 +73,21 @@ const AudioContextProvider = ({ children, audioControlHook = useAudioControl }: 
     draw(amp)
   }, [audioLevel])
 
-  const noAudioAnimation = useCallback((time: number) => {
-    const PRIMARY_ANIMATION_PERIOD = 15;
-    const SECONDARY_ANIMATION_PERIOD = 30;
-    const TERTIARY_ANIMATION_PERIOD = 60;
-    const now = time / 1000
-
-    // Normalize sin and cosine values to [0, 1] range
-    const primaryAmp = (Math.sin(2 * Math.PI * now / PRIMARY_ANIMATION_PERIOD) + 1) / 2;
-    const secondaryAmp = (Math.sin(2 * Math.PI * now / SECONDARY_ANIMATION_PERIOD) + 1) / 2;
-    const tertiaryAmp = (Math.sin(2 * Math.PI * now / TERTIARY_ANIMATION_PERIOD) + 1) / 2;
-
-    // Scale and translate to desired range, for example, [1, 1.5]
-    animate(primaryValue, primaryAmp / 2)
-    animate(secondaryValue, secondaryAmp / 2)
-    animate(tertiaryValue, tertiaryAmp / 2)
-  }, [primaryValue, secondaryValue, tertiaryValue])
-
   const shouldAnimate = !usePrefersReducedMotion() && animating
   useAnimationFrame((time) => {
     if (!shouldAnimate) return
     if (playing) {
       audioAnimation(audioData)
     } else {
-      noAudioAnimation(time)
+      animate(audioLevel, 0, {
+        duration: 0.25,
+        ease: 'easeOut'
+      })
     }
   })
 
   useMotionValueEvent(normalizedAudioLevel, 'change',
     (value) => setAmpProperty(value, 'audio'));
-  useMotionValueEvent(normalizedPrimaryLevel, 'change',
-    (value) => setAmpProperty(value, 'primary'));
-  useMotionValueEvent(normalizedSecondaryLevel, 'change',
-    (value) => setAmpProperty(value, 'secondary'));
-  useMotionValueEvent(normalizedTertiaryLevel, 'change',
-    (value) => setAmpProperty(value, 'tertiary'));
 
   useUpdateEffect(() => {
     setAnimatingTrue()
