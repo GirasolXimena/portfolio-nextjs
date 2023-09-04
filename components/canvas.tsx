1e1959ad0ad04cdcdeebbd6debf377a1761beca1
Roberto Andrade <robert.andrade@zinnia.com>
2023-09-04 20:15:26 +0000

    put canvas page on /labs/canvas

--- components/canvas.tsx
+++ components/canvas.tsx
@@ -1,34 +1,29 @@
-import { useRef, useEffect } from "react";
-import drawArrow from "./drawArrow";
+import { useRef, useEffect, CanvasHTMLAttributes, RefObject } from 'react'
+import drawArrow from './drawArrow'
+type CanvasProps = CanvasHTMLAttributes<HTMLCanvasElement> & {
+  fromx: number;
+  fromy: number;
+  tox: number;
+  toy: number;
+  headlen: number;
+  height?: number;
+  width?: number;
+  ref?: RefObject<HTMLCanvasElement>
+  
+};
 
-const Canvas = (props) => {
-  const canvasRef = useRef<HTMLCanvasElement>(null);
+const Canvas = (props: CanvasProps) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null)
 
   useEffect(() => {
-    const canvas = canvasRef.current;
-    if (!canvas) return;
-    const context = canvas.getContext("2d");
-    canvas.height = props.height;
-    canvas.width = props.width;
-    drawArrow(
-      context,
-      props.fromx,
-      props.fromy,
-      props.tox,
-      props.toy,
-      props.headlen,
-    );
-  }, [
-    props.fromx,
-    props.fromy,
-    props.tox,
-    props.toy,
-    props.headlen,
-    props.height,
-    props.width,
-  ]);
+    const canvas = props.ref?.current || canvasRef.current
+    if (!canvas) return
+    const { height, width } = canvas.getBoundingClientRect()
+    const context = canvas.getContext('2d')
+    canvas.height = props.height || height
+    canvas.width = props.width || width
+    drawArrow(context, props.fromx, props.fromy, props.tox, props.toy, props.headlen)
 
-  return <canvas ref={canvasRef} {...props} />;
-};
+  }, [props.fromx, props.fromy, props.tox, props.toy, props.headlen, props.height, props.width, props.ref])
 
 export default Canvas;
