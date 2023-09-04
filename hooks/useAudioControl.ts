5b542ef09d7ca05194cee1fc9e9395a2923f0b16
Roberto Andrade <robert.andrade@zinnia.com>
2023-09-04 22:14:29 +0000

    working through updating audi control

--- hooks/useAudioControl.ts
+++ hooks/useAudioControl.ts
@@ -3,75 +3,134 @@ import usePaletteContext from 'hooks/usePaletteContext'
 import { useBoolean, useEffectOnce, useIsClient, useUpdateEffect } from 'usehooks-ts';
 
 export type AudioDataType = MutableRefObject<AnalyserNode | null>;
+export type AudioDataTypeOption = 'frequency' | 'time' | 'both';
 
 export type UseAudioControlReturn = {
   playing: boolean;
   audioData: AudioDataType;
+  frequencyData: MutableRefObject<Uint8Array | null>;
+  timeData: MutableRefObject<Uint8Array>;
   audioElement: MutableRefObject<HTMLAudioElement | null>;
   startPlaying: () => void;
   stopPlaying: () => void;
 };
 
-const useAudioControl = (): UseAudioControlReturn => {
-  const isClient = useIsClient()
-  const { currentPalette } = usePaletteContext();
+const useAudioControl = (dataType: AudioDataTypeOption = 'both'): UseAudioControlReturn => {
   const audioElement = useRef<HTMLAudioElement | null>(null);
   const audioData = useRef<AnalyserNode | null>(null);
   const { value: playing, setTrue: setPlayingTrue, setFalse: setPlayingFalse } = useBoolean(false);
+  const isClient = useIsClient()
+  const { currentPalette } = usePaletteContext();
   const audioContext = useRef<AudioContext | null>(null);
+  const bufferRef = useRef<AudioBuffer | null>(null);
+  const frequencyData = useRef<Uint8Array>(new Uint8Array(audioData.current?.frequencyBinCount || 0));
+  const timeData = useRef<Uint8Array>(new Uint8Array(audioData.current?.frequencyBinCount || 0));
 
-  const setMusicType = useCallback((src: string) => {
-    if (audioElement.current) audioElement.current.src = src;
-  }, []);
-
-  const startPlaying = useCallback(() => {
-    if(!audioElement.current) {
-      audioElement.current = new Audio();
-      audioElement.current.src = currentPalette.palette.audio || ''
-    }
-    if (playing || !audioElement.current.src) return;
-
-    if (!audioContext.current) {
+  const initAudio = useCallback(() => {
+    if(!audioContext.current) {
+      console.log('creating new audio context')
       if (!window.AudioContext) return console.error('AudioContext not supported');
       audioContext.current = new window.AudioContext();
+      if(!audioElement.current) return console.error('No audio element found');
       const source = audioContext.current.createMediaElementSource(audioElement.current);
       const analyzer = audioContext.current.createAnalyser();
       analyzer.fftSize = 2048;
       source.connect(audioContext.current.destination);
       source.connect(analyzer);
       audioData.current = analyzer;
+      console.log('audio context created', source, analyzer)
     }
+  }, [])
 
-    if (audioElement.current) {
-      try {
-        audioElement.current.play();
-        setPlayingTrue()
-      } catch (error) {
-        console.error('Error playing audio: ', error)
+
+  const resumeAudioContext = useCallback(() => {
+    if (audioContext.current && audioContext.current.state === "suspended") {
+      audioContext.current.resume();
+    }
+  }, []);
+  
+  const loadAudio = useCallback(() => {
+    console.log('loading audio')
+    if (currentPalette.palette.audio === undefined) return console.error('No audio file found');
+    const request = new XMLHttpRequest();
+    request.open('GET', currentPalette.palette.audio, true);
+    request.responseType = 'arraybuffer';
+    request.onload = () => {
+      console.log('audio loaded', request.response)
+      if (audioContext.current) {
+        audioContext.current.decodeAudioData(request.response, (buffer) => {
+          if (!buffer) {
+            return console.error('Error decoding audio data')
+          }
+          bufferRef.current = buffer;
+          if(!audioElement.current || !audioContext.current) return console.error('No audio element found');
+          const source = audioContext.current.createBufferSource();
+          source.buffer = buffer;
+          source.connect(audioContext.current.destination);
+          source.start(0);
+        }, (error) => {
+          console.error('Error decoding audio data: ', error)
+        });
       }
     }
-  }, [playing, setPlayingTrue, currentPalette.palette.audio]);
+    request.onerror = (error) => {
+      console.error('Error loading audio data: ', currentPalette.palette.audio, error)
+    }
+    request.send();
+    if(!audioElement.current) return console.error('No audio element found');
+    audioElement.current.src = currentPalette.palette.audio;
 
-  const stopPlaying = useCallback(() => {
-    if (audioElement.current) {
+  }, [currentPalette.palette.audio]);
+
+  const updateAudioData = useCallback(() => {
+    if (!audioData.current) return;
+    if (dataType === 'frequency' || dataType === 'both') {
+      audioData.current.getByteFrequencyData(frequencyData.current);
+    }
+    if (dataType === 'time' || dataType === 'both') {
+      audioData.current.getByteTimeDomainData(timeData.current);
+    }
+  }, [audioData, dataType])
+
+
+
+
+  const togglePlayback = useCallback(() => {
+    console.log('toggle playback', playing)
+    if(!audioElement.current) return console.error('No audio element found');
+    if (!playing) {
       audioElement.current.pause();
-      setPlayingFalse()
+    } else {
+      console.log('playing audio')
+      audioElement.current.play();
+      requestAnimationFrame(updateAudioData);
     }
-  }, [setPlayingFalse]);
+  }, [updateAudioData, playing]);
 
+  // load audio on palette change
   useUpdateEffect(() => {
-    if (!isClient) return;
+    if(!isClient) return;
+    loadAudio();
+  }, [currentPalette.palette.audio])
 
-    if(audioElement.current === null) {
+  // toggle playback on play state change
+  useUpdateEffect(() => {
+    if(!isClient) return;
+    togglePlayback();
+  }, [playing])
+
+  // init audio element
+  useEffectOnce(() => {
+    console.log('effect once')
+
+    if (audioElement.current === null) {
+      console.log('creating new audio element in effect once')
       audioElement.current = new Audio();
+      initAudio()
     }
 
-    const musicType = currentPalette.palette.audio || ''
-    setMusicType(musicType)
-  }, [currentPalette, setMusicType])
-
-  useEffectOnce(() => {
     return () => {
+      console.log('cleaning up audio element')
       if (audioData.current) {
         audioData.current.disconnect();
         audioData.current = null;
@@ -88,8 +147,10 @@ const useAudioControl = (): UseAudioControlReturn => {
     playing,
     audioData,
     audioElement,
-    startPlaying,
-    stopPlaying
+    frequencyData,
+    timeData,
+    startPlaying: setPlayingTrue,
+    stopPlaying: setPlayingFalse,
   };
 };
 
