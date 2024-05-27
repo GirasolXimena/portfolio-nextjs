188392cda124c22c6e04202314d65366b0c08000
Ximena Girasol <girasol.ximena@gmail.com>
2024-05-27 03:15:00 +0000

    add lamp component

--- components/hero.tsx
+++ components/hero.tsx
@@ -2,13 +2,17 @@
 import { useMouseInput } from "hooks/useMouseInput";
 import styles from "../styles/hero.module.scss";
 import Navbar from "./navbar";
+import { LampContainer } from "./ui/lamp";
 
 export default function Hero({ name }: { name: string }) {
   useMouseInput();
   return (
-    <div className={styles.hero}>
-      <h1 id="name">{name}</h1>
-      <Navbar segment="home" />
-    </div>
+    <LampContainer>
+      <div className={styles.hero}>
+        <h1 id="name">{name}</h1>
+
+        <Navbar segment="home" />
+      </div>
+    </LampContainer>
   );
 }
