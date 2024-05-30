0b8911b94bcaa82d400593435c1057a8811bc070
Ximena Girasol <girasol.ximena@gmail.com>
2024-05-30 04:36:43 +0000

    add supporting packages

--- components/ui/hero-highlight.tsx
+++ components/ui/hero-highlight.tsx
@@ -29,14 +29,14 @@ export const HeroHighlight = ({
   return (
     <div
       className={cn(
-        "relative h-[40rem] flex items-center bg-white dark:bg-black justify-center w-full group",
+        "relative h-[40rem] flex items-center justify-center w-full group",
         containerClassName
       )}
       onMouseMove={handleMouseMove}
     >
-      <div className="absolute inset-0 bg-dot-thick-neutral-300 dark:bg-dot-thick-neutral-800 bg-repeat  pointer-events-none" />
+      <div className="absolute inset-0 bg-dot-thick-orange-300 dark:bg-dot-thick-cyan-800 bg-repeat  pointer-events-none" />
       <motion.div
-        className="pointer-events-none bg-dot-thick-indigo-500 dark:bg-dot-thick-indigo-500 bg-repeat   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
+        className="pointer-events-none bg-dot-thick-yellow-300 dark:bg-dot-thick-orange-400 bg-repeat   absolute inset-0 opacity-0 transition duration-300 group-hover:opacity-100"
         style={{
           WebkitMaskImage: useMotionTemplate`
             radial-gradient(
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
+const svgToTinyDataUri = require("mini-svg-data-uri");
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
-const svgToDataURL = require("svg-to-dataurl");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -39,7 +39,7 @@ module.exports = {
       matchUtilities(
         {
           "bg-dot-thick": (value: any) => ({
-            backgroundImage: `url("${svgToDataURL(
+            backgroundImage: `url("${svgToTinyDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="2.5"></circle></svg>`
             )}")`,
           }),
