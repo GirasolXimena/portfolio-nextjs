3fa1357973cebb179f8042db01f414778e4693a8
Ximena Girasol <girasol.ximena@gmail.com>
2024-06-13 00:22:57 +0000

    import spectrum styles

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,13 @@
+import React from 'react'
 import type { Preview } from "@storybook/react";
+import '../styles/global.scss'
 
 const preview: Preview = {
+  decorators: [
+    (Story) => (
+      <div className='spectrum spectrum--light spectrum--large'><Story /></div>
+    )
+  ],
   parameters: {
     controls: {
       matchers: {
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,6 +1,11 @@
 import {Button as RACButton, ButtonProps} from 'react-aria-components';
-import './Button.css';
+/*
+  Recommended: For components with multiple contexts available, if you
+  want access to all contexts, load the index.css file, which includes
+  all contexts and component variables.
+*/
+import "node_modules/@spectrum-css/button/dist/index.css";
 
 export function Button(props: ButtonProps) {
-  return <RACButton {...props} />;
+  return <RACButton className={'spectrum-Button spectrum-Button--fill spectrum-Button--primary spectrum-Button--sizeS'} {...props} />;
 }
